0002a37bb93dc5f621b741ed9026b357992a2255
Mary <kass.o@yandex.ru>
2023-11-09 16:00:07 +0300

    functions were written for code implementation

---
+++ src/functions.py
@@ -0,0 +1,31 @@
+import json
+from typing import Any, Optional, List, Dict
+import datetime
+
+
+def getting_data_from_file(filename: Optional) -> List[Dict[str, Any]]:
+    """ считывает из файла данные """
+    with open(filename, encoding="utf8") as file:
+        return json.load(file)
+
+
+def filtering_sorting_list(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """ Принимает список данных и выводит только проведенные 5 последние операции в виде списка словарей"""
+    list_to_be_sorted = (item for item in data if item.get('state') == 'EXECUTED')
+    return sorted(list_to_be_sorted, key=lambda d: d['date'])[-5:]
+
+
+def card_number(number_card: str) -> str:
+    """ Функция принимает строку, вычисляет по длине номер или счет и выводит замаскированный тип и номер """
+    num = "".join(n for n in number_card if n.isdecimal())
+    type_num = "".join(s for s in number_card if s.isalpha())
+    if len(num) == 16:
+        return f"{type_num} {num[0:4]} {num[4:6]}** **** {num[12:17]}"
+    if len(num) == 20:
+        return f"{type_num} **{num[16:21]}"
+
+
+def date_converter(str_date: str) -> datetime:
+    """ Функция принимает строку и выдает дату в формате %d-%m-%Y """
+    date_time_str = str_date[:10]
+    return datetime.datetime.strptime(date_time_str, '%Y-%m-%d').strftime('%d-%m-%Y')
